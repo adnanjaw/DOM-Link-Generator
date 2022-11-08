e3cde327af423c4123c4adc8a578d62138cbb1e1
Adnan al Jawabra <ajawabra@senet.nl>
2022-11-08 16:17:06 +0100

    options page

--- options.js
+++ options.js
@@ -1,9 +1,41 @@
 $(function () {
-    chrome.storage.local.get(['jiraLink'], (result) => {
+    chrome.storage.local.get(['jiraLink', 'dynamicLink'], (result) => {
         if (chrome.runtime.lastError)
             console.log('Error getting');
 
-        $('#jira-link').val(result.jiraLink)
+        console.log(result);
+        // $('#jira-link').val(result.jiraLink)
+
+
+        // let rowIdx = 0;
+        // $('#dynamic-link-table').append(`<tr id="id-${++rowIdx}">
+        //         <th scope="row">${rowIdx}</th>
+        //         <td>${result.dynamicLink.name}</td>
+        //         <td>${result.dynamicLink.url}</td>
+        //         <td>${result.dynamicLink.azuerField}</td>
+        //         <td>
+        //             <button type="button" class="btn btn-danger">Delete</button>
+        //         </td>
+        //     </tr>`);
+    });
+
+    $('#dynamic-link-form').submit(function (event) {
+        event.preventDefault();
+        let dynamicLink = {
+            name: $('#dynamic-link-name').val(),
+            url: $('#dynamic-link-url').val(),
+            azuerField: $('#dynamic-azuer-planning-field-title').val()
+        }
+        let dynamicLinkCollection = [];
+        dynamicLinkCollection.push(dynamicLink);
+        chrome.storage.local.set({ 'dynamicLinkCollection': dynamicLinkCollection }, () => {
+            if (chrome.runtime.lastError)
+                console.log('Error setting');
+
+            alert('New dynamic link is stored.');
+            // close();
+        });
+
     });
 
 
@@ -16,7 +48,7 @@ $(function () {
                     console.log('Error setting');
 
                 alert('jiraLink is stored');
-                close();
+                // close();
             });
         };
     })
