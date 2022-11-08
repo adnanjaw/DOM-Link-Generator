a3e06ee178e1639976ff5f2fb06b7ed40b59b917
Adnan al Jawabra <ajawabra@senet.nl>
2022-11-08 16:57:35 +0100

    dynamic & static links options

--- options.js
+++ options.js
@@ -1,55 +1,98 @@
-$(function () {
-    chrome.storage.local.get(['jiraLink', 'dynamicLink'], (result) => {
-        if (chrome.runtime.lastError)
-            console.log('Error getting');
+const options = {};
 
-        console.log(result);
-        // $('#jira-link').val(result.jiraLink)
+$(async function () {
 
+    await getOptionsFromStorageAsync;
 
-        // let rowIdx = 0;
-        // $('#dynamic-link-table').append(`<tr id="id-${++rowIdx}">
-        //         <th scope="row">${rowIdx}</th>
-        //         <td>${result.dynamicLink.name}</td>
-        //         <td>${result.dynamicLink.url}</td>
-        //         <td>${result.dynamicLink.azuerField}</td>
-        //         <td>
-        //             <button type="button" class="btn btn-danger">Delete</button>
-        //         </td>
-        //     </tr>`);
-    });
+    await getDynamicLinksCollectionAsync();
 
-    $('#dynamic-link-form').submit(function (event) {
+    await getStaticLinksCollectionAsync();
+
+    $('#dynamic-link-form').submit(async function (event) {
+
+        await getOptionsFromStorageAsync;
+
         event.preventDefault();
         let dynamicLink = {
             name: $('#dynamic-link-name').val(),
             url: $('#dynamic-link-url').val(),
             azuerField: $('#dynamic-azuer-planning-field-title').val()
         }
-        let dynamicLinkCollection = [];
+        let dynamicLinkCollection = jQuery.isEmptyObject(options.dynamicLinkCollection) ? [] : options.dynamicLinkCollection;
+
         dynamicLinkCollection.push(dynamicLink);
         chrome.storage.local.set({ 'dynamicLinkCollection': dynamicLinkCollection }, () => {
             if (chrome.runtime.lastError)
                 console.log('Error setting');
-
-            alert('New dynamic link is stored.');
-            // close();
         });
+
+        await getDynamicLinksCollectionAsync();
 
     });
 
+    $('#static-link-form').submit(async function (event) {
 
-    // add jiraLink
-    $('#jira-link-submit').click(function () {
-        let jiraLink = $('#jira-link').val();
-        if (jiraLink) {
-            chrome.storage.local.set({ 'jiraLink': jiraLink }, () => {
-                if (chrome.runtime.lastError)
-                    console.log('Error setting');
+        await getOptionsFromStorageAsync;
 
-                alert('jiraLink is stored');
-                // close();
-            });
-        };
-    })
+        event.preventDefault();
+        let staticLink = {
+            name: $('#static-link-name').val(),
+            url: $('#static-link-url').val(),
+        }
+        let staticLinkCollection = jQuery.isEmptyObject(options.staticLinkCollection) ? [] : options.staticLinkCollection;
+
+        staticLinkCollection.push(staticLink);
+        chrome.storage.local.set({ 'staticLinkCollection': staticLinkCollection }, () => {
+            if (chrome.runtime.lastError)
+                console.log('Error setting');
+        });
+
+        await getStaticLinksCollectionAsync();
+
+    });
+
 })
+
+const getOptionsFromStorageAsync = getAllStorageLocalData().then(items => {
+    Object.assign(options, items);
+});
+
+async function getDynamicLinksCollectionAsync() {
+    $("#dynamic-link-table").empty();
+    $.each(options.dynamicLinkCollection, function (index, dynamicLink) {
+        $('#dynamic-link-table').append(`<tr id="id-${++index}">
+                <th scope="row">${index}</th>
+                <td>${dynamicLink.name}</td>
+                <td>${dynamicLink.url}</td>
+                <td>${dynamicLink.azuerField}</td>
+                <td>
+                    <button type="button" class="btn btn-danger">Delete</button>
+                </td>
+            </tr>`);
+    });
+}
+
+async function getStaticLinksCollectionAsync() {
+    $("#static-link-table").empty();
+    $.each(options.staticLinkCollection, function (index, staticLink) {
+        $('#static-link-table').append(`<tr id="id-${++index}">
+                <th scope="row">${index}</th>
+                <td>${staticLink.name}</td>
+                <td>${staticLink.url}</td>
+                <td>
+                    <button type="button" class="btn btn-danger">Delete</button>
+                </td>
+            </tr>`);
+    });
+}
+
+function getAllStorageLocalData() {
+    return new Promise((resolve, reject) => {
+        chrome.storage.local.get(null, (items) => {
+            if (chrome.runtime.lastError) {
+                return reject(chrome.runtime.lastError);
+            }
+            resolve(items);
+        });
+    });
+}
